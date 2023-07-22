62d0e294f258039a44d87ea69b2d8f0cf04fb968
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-07-22 19:35:41 -0300

    QLineEdit e o display

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import sys
 
 from main_window import MainWindow
 from PySide6.QtWidgets import QApplication
+from display import Display
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    # Display
+    display = Display()
+    display.setPlaceholderText('0')
+    window.addToVLayout(display)
 
     # Executa tudo
     window.adjustFixedSize()
