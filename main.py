7a054fa2b5eb52ae4d537e2b364564c7e1bac724
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-07-24 10:55:33 -0300

    criando um QLabel para mostrar informações

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import sys
 from main_window import MainWindow
 from PySide6.QtWidgets import QApplication
 from display import Display
+from info import Info
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    # Info
+    info = Info('0.0 * 0.0 = 0.0')
+    window.addToVLayout(info)
 
     # Display
     display = Display()
