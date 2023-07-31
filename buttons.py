5f44afc99d4071120d8dad3df82bac3adb2c928c
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-07-31 15:28:38 -0300

    criando um Slot com dados para o Signal "clicked" de cada botão"

--- buttons.py
+++ buttons.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
+from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE
 from utils import isNumOrDot, isEmpty
+from display import Display
 
 
 class Button(QPushButton):
@@ -16,7 +18,7 @@ class Button(QPushButton):
 
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, display: Display, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -26,6 +28,7 @@ class ButtonsGrid(QGridLayout):
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
+        self.display = display
         self._makeGrid()
 
     def _makeGrid(self):
@@ -35,3 +38,18 @@ class ButtonsGrid(QGridLayout):
                 if not isNumOrDot(buttonText) and not isEmpty(buttonText):
                     button.setProperty('cssClass', 'specialButton')
                 self.addWidget(button, i, j)
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button,
+                )
+                button.clicked.connect(buttonSlot)
+
+    def _makeButtonDisplaySlot(self, method, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            method(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextToDisplay(self, button):
+        buttonText = button.text()
+        self.display.insert(buttonText)
