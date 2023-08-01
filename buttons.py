afe8b97b077064350ffbf8996b88a761a96a0be6
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-08-01 14:08:51 -0300

    permitindo apenas números válidos no display

--- buttons.py
+++ buttons.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
 from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE
-from utils import isNumOrDot, isEmpty
+from utils import isNumOrDot, isEmpty, isValidNumber
 from display import Display
 
 
@@ -52,4 +52,9 @@ class ButtonsGrid(QGridLayout):
 
     def _insertButtonTextToDisplay(self, button):
         buttonText = button.text()
+        newDisplayValue = self.display.text() + buttonText
+
+        if not isValidNumber(newDisplayValue):
+            return
+
         self.display.insert(buttonText)
